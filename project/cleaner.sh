619e44eab0a89a5f103846ae468779d387ca5742
Eugene Kapelko <softwarearchitect19@gmail.com>
2024-08-17 15:59:22 +0300

    Create docker files.

---
+++ project/cleaner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PORT=8080
+
+PID=$(lsof -t -i:$PORT)
+
+if [ -z "$PID" ]; then
+  echo "No process is using port $PORT."
+else
+  kill -9 $PID
+  echo "Process $PID using port $PORT has been killed."
+fi
