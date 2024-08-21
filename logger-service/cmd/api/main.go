3dc1b450fd65e34deb7587514e1f3e6bd204b6f1
Eugene Kapelko <softwarearchitect19@gmail.com>
2024-08-21 22:18:34 +0300

    Use RPC.

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -7,7 +7,9 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 	"log-service/data"
+	"net"
 	"net/http"
+	"net/rpc"
 	"time"
 )
 
@@ -45,7 +47,9 @@ func main() {
 		Models: data.New(client),
 	}
 
-	//go app.serve()
+	// Register the RPC Server
+	err = rpc.Register(new(RPCServer))
+	go app.rpcListen()
 
 	log.Println("Starting server...")
 	srv := &http.Server{
@@ -59,17 +63,23 @@ func main() {
 	}
 }
 
-//func (app *Config) serve() {
-//	srv := &http.Server{
-//		Addr:    fmt.Sprintf(":%s", webPort),
-//		Handler: app.routes(),
-//	}
-//
-//	err := srv.ListenAndServe()
-//	if err != nil {
-//		log.Panic(err)
-//	}
-//}
+func (app *Config) rpcListen() error {
+	log.Println("Starting rpc server...")
+	listen, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%s", rpcPort))
+	if err != nil {
+		return err
+	}
+	defer listen.Close()
+
+	for {
+		rpcConn, err := listen.Accept()
+		if err != nil {
+			return err
+		}
+		go rpc.ServeConn(rpcConn)
+	}
+
+}
 
 func connectToMongo() (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI(mongoURL)
